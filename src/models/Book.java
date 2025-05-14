5aef70a057c813e8036bf93cb1b1bcbe14caad4b
Tiago Baptista Noronha <tiago.bn@gmail.com>
2025-05-13 22:11:24 -0300

    Book class creation.

---
+++ src/models/Book.java
@@ -0,0 +1,59 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Book {
+
+    private final Long id;
+    private String title;
+    private boolean available;
+    private final LocalDate dateCreation;
+    private LocalDate dateLastUpdate;
+
+    public Book(Long id, String title) {
+        this.id = id;
+        this.title = title;
+        available = true;
+        dateCreation = dateLastUpdate = LocalDate.now();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public LocalDate getDateCreation() {
+        return dateCreation;
+    }
+
+    public LocalDate getDateLastUpdate() {
+        return dateLastUpdate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        refreshDateLastUpdate();
+    }
+
+    public void borrow() {
+        available = false;
+        refreshDateLastUpdate();
+    }
+
+    public void giveBack() {
+        available = true;
+        refreshDateLastUpdate();
+    }
+
+    private void refreshDateLastUpdate() {
+        dateLastUpdate = LocalDate.now();
+    }
+}
