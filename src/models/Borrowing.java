fa756ef5ed76d3ee64b6a27e8db43428520faddd
Tiago Baptista Noronha <tiago.bn@gmail.com>
2025-05-13 23:03:06 -0300

    Creation of Borrowing Class.

---
+++ src/models/Borrowing.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Borrowing {
+
+    private static Long lastId = 0L;
+    private Long id;
+    private Book book;
+    private String user;
+    private LocalDate dateBorrow;
+    private LocalDate dateGiveBack;
+
+    public Borrowing(Book book, String user){
+        id = ++lastId;
+        this.book = book;
+        this.user = user;
+        dateBorrow = LocalDate.now();
+        dateGiveBack = null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public LocalDate getDateBorrow() {
+        return dateBorrow;
+    }
+
+    public LocalDate getDateGiveBack() {
+        return dateGiveBack;
+    }
+
+    public boolean isActive(){
+        return  dateGiveBack==null;
+    }
+
+
+}
