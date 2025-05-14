1950de99008ee8d1b725b867c9e1bf7fb71c0b67
Tiago Baptista Noronha <tiago.bn@gmail.com>
2025-05-13 22:53:15 -0300

    Creation of Library class

---
+++ src/models/Library.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private final List<Book> books = new ArrayList<>();
+    private final List<Author> authors = new ArrayList<>();
+
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+    public void addAuthor(Author author){
+        authors.add(author);
+    }
+
+    public List<Book> getAvailableBooks(){
+        return books.stream().filter((book)->book.isAvailable()).toList();
+    }
+
+    public List<Author> getAuthors(){
+        return new ArrayList<>(authors);
+    }
+
+}
